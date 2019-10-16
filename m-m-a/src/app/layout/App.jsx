c5fa5cb2828a12e31394065464f042bcb59c5d5a
Jack Maldonado <jackmaldonado@Jacks-MBP.attlocal.net>
2019-10-16 11:17:46 -0700

    file structure

---
+++ m-m-a/src/app/layout/App.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>MMA Central</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
